c716c49c2e24a01feaa150e447b64c9e1ed08c6b
Victor Caldas <caldas.victor@gmail.com>
2018-01-26 09:46:46 +0100

    Introduction Java Datatypes

--- java/introduction/_08_Java_Datatypes.java
+++ java/introduction/_08_Java_Datatypes.java
@@ -9,13 +9,42 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/java-datatypes/problem
  */
 public class _08_Java_Datatypes {
-	private static File f = new File("resources/introduction/input6.txt");
+	private static File f = new File("resources/introduction/input7.txt");
 	private static Scanner in = null ;
 	
 	private static boolean SUBMIT = false;
 	
 	private static void solution() {
-		
+		int t=in.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=in.nextLong();
+                System.out.println(x+" can be fitted in:");
+                //byte, short, int, and long):
+                /*
+                 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+                 */
+                // It has a minimum value of -128 and a maximum value of 127 (inclusive)
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)System.out.println("* byte");
+                //It has a minimum value of -32,768 and a maximum value of 32,767 (inclusive).
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)System.out.println("* short");
+                //minimum value of -2^31 and a maximum value of 2^31 -1.
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)System.out.println("* int");
+                //2^63- and a maximum value of 2^63-1.
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)System.out.println("* long");
+                
+                //Complete the code
+            }
+            catch(Exception e)
+            {
+                System.out.println(in.next()+" can't be fitted anywhere.");
+            }
+
+        }
 		
 	}
 
